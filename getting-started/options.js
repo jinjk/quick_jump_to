275fe691fa88ac3d3ea3798f04199450141b0fd2
jiankun.jin <jiankun.jin@gmail.com>
2021-11-28 11:33:10 +0800

    add load/restore method

---
+++ getting-started/options.js
@@ -0,0 +1,80 @@
+var globalConfig = [];
+
+function getJiraSiteUrl() {
+    jiraSiteUrl = '';
+    for (item of globalConfig) {
+        if (item.index == 1) {
+            jiraSiteUrl = item.url;
+            break;
+        }
+    }
+    return jiraSiteUrl;
+}
+
+function updateJiraSiteUrl(jiraUrl) {
+    found = false;
+    for (item of globalConfig) {
+        if (item.index == 1) {
+            item.url = jiraUrl;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        item = {
+            index: 1,
+            url: jiraUrl,
+            regex: '.*'
+        }
+        globalConfig.push(item);
+    }
+}
+
+// Saves options to chrome.storage
+function save_options() {
+    var jiraSiteUrl = document.getElementById('input_site_url').value;
+    var jsonStr = document.getElementById('input_config').value;
+    if (jsonStr) {
+        try {
+            globalConfig = JSON.parse(jsonStr);
+        }
+        catch (e) {
+            var status = document.getElementById('status');
+            status.textContent = 'Error in json config, please correct.';
+            setTimeout(function () {
+                status.textContent = '';
+            }, 750);
+            return;
+        }
+    }
+    updateJiraSiteUrl(jiraSiteUrl);
+    jsonStr = JSON.stringify(globalConfig);
+    chrome.storage.sync.set({
+        configDoc: jsonStr
+    }, function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function () {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value jiraSiteUrl = 'red' and configDoc = true.
+    chrome.storage.sync.get({
+        configDoc: '[]'
+    }, function (items) {
+        globalConfig = JSON.parse(items.configDoc);
+        var jiraSiteUrl = getJiraSiteUrl();
+        document.getElementById('jiraSiteUrl').value = jiraSiteUrl;
+        document.getElementById('input_config').value = items.configDoc;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
