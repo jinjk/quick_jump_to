a37c56202d9f546637d5c6c15f6c863acba6aa90
c5241628 <jiankun.jin@sap.com>
2021-11-29 13:38:46 +0800

    fix ui update issue

--- getting-started/options.js
+++ getting-started/options.js
@@ -3,30 +3,24 @@ var globalJiraSiteUrl = '';
 
 function getJiraSiteUrl() {
     jiraSiteUrl = '';
-    for (item of globalConfig) {
-        if (item.index == 1) {
-            jiraSiteUrl = item.url;
-            break;
-        }
+    if (globalConfig.length > 0) {
+        let item = globalConfig[0];
+        jiraSiteUrl = item.url;
     }
     return jiraSiteUrl;
 }
 
 function updateJiraSiteUrl(jiraUrl) {
-    found = false;
-    for (item of globalConfig) {
-        if (item.index == 1) {
-            item.url = jiraUrl;
-            found = true;
-            break;
-        }
+    if (globalConfig.length > 0) {
+        let item = globalConfig[0];
+        item.url = jiraUrl;
+        found = true;
     }
-
-    if (!found) {
+    else {
         item = {
             index: 1,
             url: jiraUrl,
-            regex: '^.*$'
+            regex: '\\w+-\\w+'
         }
         globalConfig.push(item);
     }
@@ -36,16 +30,16 @@ function updateJiraSiteUrl(jiraUrl) {
 function save_options() {
     var jiraSiteUrl = document.getElementById('input_site_url').value;
     var jsonStr = document.getElementById('input_config').value;
+    var status = document.getElementById('status');
+    status.textContent = '';
     if (jsonStr) {
         try {
             globalConfig = JSON.parse(jsonStr);
+            globalConfig = globalConfig.sort((a, b) => a.index - b.index);
         }
         catch (e) {
-            var status = document.getElementById('status');
-            status.textContent = 'Error in json config, please correct.';
-            setTimeout(function () {
-                status.textContent = '';
-            }, 750);
+            status.setAttribute("messageType", "error");
+            status.textContent = "Error in json config, please correct. (regex uses double backslash for '\\')";
             return;
         }
     }
@@ -63,11 +57,8 @@ function save_options() {
         configDoc: jsonStr
     }, function () {
         // Update status to let user know options were saved.
-        var status = document.getElementById('status');
+        status.setAttribute("messageType", "info");
         status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 750);
     });
 }
 
@@ -79,6 +70,7 @@ function restore_options() {
         configDoc: '[]'
     }, function (items) {
         globalConfig = JSON.parse(items.configDoc);
+        globalConfig = globalConfig.sort((a, b) => a.index - b.index);
         globalJiraSiteUrl = getJiraSiteUrl();
         document.getElementById('input_site_url').value = globalJiraSiteUrl;
         document.getElementById('input_config').value = items.configDoc;
